91e99c091cb2400ace7ebb14d6316019a4496274
MR.IBAY <ibayjimwell@gmail.com>
2025-02-16 15:22:48 +0800

    I start build the main page

--- controllers/login.php
+++ controllers/login.php
@@ -6,4 +6,4 @@ $database = new Database(require '../config.php');
 $email = $_POST['email'];
 $user = $database->query('SELECT * FROM users WHERE email = ?', [$email]);
 
-echo ($user[0]['username']);
+require '../views/main.view.php';
--- views/main.view.php
+++ views/main.view.php
@@ -1,5 +1,24 @@
 <?php require 'partials/header.view.php' ?>
 
-<h1>Notes</h1>
+<div class="flex flex-cols justify-center gap-4 mt-16">
+    <div>
+        <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </a>
+    </div>
+    <div>
+        <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </a>
+    </div>
+    <div>
+        <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </a>
+    </div>
+</div>
 
 <?php require 'partials/footer.view.php' ?>
